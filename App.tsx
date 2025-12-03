ce8acabcc006fea170844f14025986e92f0c418b
Pedro Santos <pedrogsantosdev@outlook.com>
2025-12-03 00:47:40 -0300

    chore: alterando a configuração do eas.json para deploy

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { PaperProvider } from "react-native-paper";
 import { Layout } from "./src/components/Layout/Layout";
-import React from "react";
+import React, { useEffect } from "react";
 import { AppProvider } from "@/src/context/AppStorageContext";
 import { NotificationProvider } from "@/src/providers/NotificationProvider";
+import NotificationService from "./src/service/notifications/NotificationService";
 
 export default function App() {
+  useEffect(() => {
+    NotificationService.init();
+  }, []);
   return (
     <NotificationProvider>
       <PaperProvider>
